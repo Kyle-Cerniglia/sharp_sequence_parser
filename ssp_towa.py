621287feeed4dbd98a3d56cfdd083fcbb5cea7f6
cernigliakyle@gmail.com <cernigliakyle@gmail.com>
2024-12-18 20:03:57 -0700

    combined everything into the SSP_towa file

--- ssp_towa.py
+++ ssp_towa.py
@@ -1,15 +1,87 @@
-# Parser for the sharpcap sequencer (Towa telescope)
+# Parser for the sharpcap sequencer
 
 import sys
 import math
+from enum import Enum
+from enum import auto
+
+class Telescope(Enum):
+    ORION = 1
+    TOWA = 2
+    
+class Filters(Enum):
+    UVIR = 1
+    LENHANCE = 2
+    LPRO = 3
+
+# Exposure time
+EXPOSURE_ORION = {
+    Filters.UVIR: 30,
+    Filters.LENHANCE: 180
+}
+EXPOSURE_TOWA = {
+    Filters.UVIR: 60,
+    Filters.LPRO: 180
+}
+EXPOSURE = {
+    Telescope.ORION: EXPOSURE_ORION,
+    Telescope.TOWA: EXPOSURE_TOWA
+}
+
+# Platesolving exposure time
+PLATE_EXPOSURE_ORION = {
+    Filters.UVIR: 4,
+    Filters.LENHANCE: 4
+}
+PLATE_EXPOSURE_TOWA = {
+    Filters.UVIR: 12,
+    Filters.LPRO: 12
+}
+PLATE_EXPOSURE = {
+    Telescope.ORION: PLATE_EXPOSURE_ORION,
+    Telescope.TOWA: PLATE_EXPOSURE_TOWA
+}
+
+# Time divider for frame calculation
+TIMEDIV_ORION = {
+    Filters.UVIR: 35.08,
+    Filters.LENHANCE: 188
+}
+TIMEDIV_TOWA = {
+    Filters.UVIR: 70.16,
+    Filters.LPRO: 181.73
+}
+TIMEDIV = {
+    Telescope.ORION: TIMEDIV_ORION,
+    Telescope.TOWA: TIMEDIV_TOWA
+}
+
+# Frames per dither
+DITHER_ORION = {
+    Filters.UVIR: 12,
+    Filters.LENHANCE: 6
+}
+DITHER_TOWA = {
+    Filters.UVIR: 10,
+    Filters.LPRO: 6
+}
+DITHER = {
+    Telescope.ORION: DITHER_ORION,
+    Telescope.TOWA: DITHER_TOWA
+}
 
 class Session:
     def __init__(
-        self, outfile, temperature, filter_type
+        self, outfile, temperature, filter_type, telescope_type, exposure_time, timediv, dither, plate_exposure_time
     ):
         self.outfile = outfile
         self.temperature = temperature
         self.filter_type = filter_type
+        self.telescope_type = telescope_type
+        self.exposure_time = exposure_time
+        self.timediv = timediv
+        self.dither = dither
+        self.plate_exposure_time = plate_exposure_time
     
     def start_time(self) -> None:
         #Set start time
@@ -35,8 +107,19 @@ class Session:
         #Set cooler temperature
         self.temperature = input("Set cooler temp C (100 to disable)\n")
         
-    def set_filter(self) -> None:    
-        self.filter_type = input("Is the filter (b)roadband (UV/IR Cut) or (n)arrowband (L-Pro)?\n")
+    def set_telescope(self) -> None:
+        scope_in = input("Select your telescope:\n[1] = Orion 130ST\n[2] = Towa 339\n")
+        self.telescope_type = Telescope(int(scope_in))
+        
+    def set_filter(self) -> None:
+        filter_in = input("Select your filter::\n[1] = UV/IR\n[2] = L-Enhance\n[3] = L-Pro\n")
+        self.filter_type = Filters(int(filter_in))
+    
+    def calc_capture_vals(self) -> None:
+        self.exposure_time = EXPOSURE[self.telescope_type][self.filter_type]
+        self.plate_exposure_time = PLATE_EXPOSURE[self.telescope_type][self.filter_type]
+        self.timediv = TIMEDIV[self.telescope_type][self.filter_type]
+        self.dither = DITHER[self.telescope_type][self.filter_type]
 
     def create_target(self) -> None:
         #Setup
@@ -50,9 +133,9 @@ class Session:
         #Configure image formatting
         self.outfile.write("    SET COLOUR SPACE TO RAW16\n")
         self.outfile.write("    SET OUTPUT FORMAT TO \"FITS files (*.fits)\"\n")
-        if self.filter_type == 'b':
+        if self.filter_type == Filters.UVIR:
             self.outfile.write("    LOAD PROFILE \"533 OSC\"\n")
-        if self.filter_type == 'n':
+        else:
             self.outfile.write("    LOAD PROFILE \"533 HO\"\n")
             
         self.outfile.write("    MOUNT CONNECT\n")
@@ -70,14 +153,11 @@ class Session:
         #Set target name
         target_name = input("Enter target name\n")
         self.outfile.write("    TARGETNAME \"" + target_name + "\"\n")
-        if self.filter_type == 'b':
-            self.outfile.write("    SET EXPOSURE TO 60\n")
-        if self.filter_type == 'n':
-            self.outfile.write("    SET EXPOSURE TO 180\n")
+        self.outfile.write("    SET EXPOSURE TO " + str(self.exposure_time) + "\n")
 
         #Platesolve and correct position
         self.outfile.write("    PRESERVE CAMERA SETTINGS\n")
-        self.outfile.write("        SET EXPOSURE TO 12\n")
+        self.outfile.write("        SET EXPOSURE TO " + str(self.plate_exposure_time) + "\n")
         self.outfile.write("        SET GAIN TO 100\n")
         self.outfile.write("        MOUNT SOLVEANDSYNC\n")
         self.outfile.write("    END PRESERVE\n")
@@ -89,17 +169,10 @@ class Session:
         self.outfile.write("    DELAY 20\n")
         self.outfile.write("    PRESERVE CAMERA SETTINGS\n")
         self.outfile.write("        FRAMETYPE Light\n")
-        if self.filter_type == 'b':
-            self.outfile.write("        GUIDING DITHER EVERY 10 FRAMES\n")
-        else:#Assuming narrowband
-            self.outfile.write("        GUIDING DITHER EVERY 6 FRAMES\n")
+        self.outfile.write("        GUIDING DITHER EVERY " + str(self.dither) + " FRAMES\n")
         frame_duration = input("Enter number of hours to capture data\n")
-        if self.filter_type == 'b':
-            frame_qty = (float(frame_duration) * 3600) / 70.16
-            frame_qty = math.floor(frame_qty)
-        else:#Assuming narrowband
-            frame_qty = (float(frame_duration) * 3600) / 181.73
-            frame_qty = math.floor(frame_qty)
+        frame_qty = (float(frame_duration) * 3600) / self.timediv
+        frame_qty = math.floor(frame_qty)
             
         frame_qty_s = str(frame_qty)
         self.outfile.write("        CAPTURE " + frame_qty_s + " FRAMES REQUIREGUIDING True\n")
@@ -127,13 +200,17 @@ def main() -> None:
     filename = input("Set filename\n")
     fileout = open(filename, "w+")
 
-    session = Session(fileout, 100, "b")
+    session = Session(fileout, 100, Filters.UVIR, Telescope.ORION, 0, 0, 0, 0)
 
     session.start_time()
     
     session.set_temp()
     
+    session.set_telescope()
+    
     session.set_filter()
+    
+    session.calc_capture_vals()
     
     session.unpark()
 
